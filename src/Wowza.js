b66054ea8b2fb70c7e455e9ff319ffed37da5d8e
Anastasiia <nastenka9410@gmail.com>
2021-03-15 12:09:37 +0200

    improved requests

--- src/Wowza.js
+++ src/Wowza.js
@@ -6,8 +6,19 @@ import ReactPlayer from 'react-player'
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Typography from '@material-ui/core/Typography';
-import { createStream, startStream, stopStream, getAll, getStream } from './api';
+import { 
+  createStream, startStream, stopStream, 
+  getAll, getStream, patchStream, getStreamState,
+  checkVideo,
+} from './api';
+// https://api.docs.cloud.wowza.com/current/tag/live_streams/#operation/createLiveStream
 
+const LIVE_STREAM_STATES = {
+  STOPPED: 'stopped',
+  RESETING: 'resetting',
+  STARTING: 'starting',
+  STARTED: 'started',
+}
 const MyButton = styled(({ color, ...other }) => <Button {...other} />)({
   background: (props) => {
     if (props.color === 'red')
@@ -25,6 +36,7 @@ const MyButton = styled(({ color, ...other }) => <Button {...other} />)({
       ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
       : '0 3px 5px 2px rgba(33, 203, 243, .3)',
   color: 'white',
+  textTransform: 'none',
 });
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -47,50 +59,95 @@ const Wowza = () => {
   const handleCreate = async () => {
     const { live_stream } = await createStream();
     console.log('create ', live_stream);
-    const { player_hls_playback_url, id, name, source_connection_information } = live_stream;
+    const {
+      player_hls_playback_url,
+      id, 
+      name, 
+      source_connection_information,
+      player_embed_code,
+      player_id,
+    } = live_stream;
     setStream({
       hls: player_hls_playback_url,
       id,
       name,
       source: source_connection_information,
+      embed: player_embed_code,
+      player: player_id,
     })
   }
 
   const handleStart = async () => {
     const result = await startStream(stream.id);
     console.log('handleStart ', result);
-    setStream({ ...stream, started: true });
+    // setStream({ ...stream, started: true });
   }
 
   const handleStop = async () => {
     const result = await stopStream(stream.id);
     console.log('handleStart ', result);
-    setStream({ ...stream, started: false });
+    setStream({ ...stream, started: false, state: result.state });
   }
 
   const handleGetAll = async () => {
     const { live_streams } = await getAll();
     console.log('handleGetAll ', live_streams);
-    const { id, name } = live_streams[4];    
+    const { id, name } = live_streams[3];    
     setStream({
+      ...stream,
       id,
       name,
+    })
+  }
+
+  const getLiveState = async () => {
+    const { live_stream } = await getStreamState(stream.id);
+    console.log('GET State ', live_stream.state, live_stream);
+    setStream({
+      ...stream, state: live_stream?.state,
     })
   }
 
   const handleGetOne = async () => {
     const { live_stream } = await getStream(stream.id);
     console.log('handleGetOne ', live_stream);
-    const { player_hls_playback_url, id, name, source_connection_information } = live_stream;    
+    const { 
+      player_hls_playback_url, id, name, source_connection_information,
+      player_embed_code,
+      player_id,
+      stream_targets,
+    } = live_stream;    
     setStream({
       hls: player_hls_playback_url,
       id,
       name,
       source: source_connection_information,
+      embed: player_embed_code,
+      player: player_id,
+      tr: stream_targets[0]?.id,
     })
   }
 
-  console.log('stream ', stream);
+  const handleUpdate = async () => {
+    const result = await patchStream(stream.id, 
+      { "live_stream": { "disable_authentication": true }});
+    console.log('handleUpdate', result);
+  }
+
+  const refreshPlayer = (started) => {
+    console.log('Refresh Player ', started)
+    setStream({ ...stream, started });
+  }
+
+  const getVideoStreamState = async () => {
+    // const result = await getVideoStream(stream.hls);
+    // console.log('getVideoStreamState', result);
+    window.player = player;
+    console.log('player ',player);
+    const started = await checkVideo(stream.hls);
+    refreshPlayer(started);
+ 
+  }
 
   return (
     <>
@@ -101,28 +158,46 @@ const Wowza = () => {
         </Typography>
           <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
             <MyButton color='blue' onClick={handleGetAll}>Get All</MyButton>
-            <MyButton color='blue' onClick={handleGetOne} disabled={!stream}>Get One</MyButton>
+            <MyButton color='green' onClick={handleGetOne} disabled={!stream}>Get One</MyButton>
+            <MyButton color='red' onClick={handleUpdate} disabled={!stream}>Update Stream</MyButton>
             <MyButton color='blue' onClick={handleCreate}>Create</MyButton>
-            <MyButton color='green' onClick={handleStart} disabled={!stream}>Start</MyButton>
-            <MyButton color='red' onClick={handleStop} disabled={!stream || !stream.started}>Stop</MyButton>
+            <MyButton color='green' onClick={handleStart} disabled={!stream || stream.state !== LIVE_STREAM_STATES.STOPPED}>Start</MyButton>
+            <MyButton color='red' onClick={handleStop} disabled={!stream || !stream.started || stream.state === LIVE_STREAM_STATES.STOPPED}>Stop</MyButton>
+            <MyButton color='blue' onClick={getLiveState} disabled={!stream}>Get Live State</MyButton>
+            <MyButton color='blue' onClick={refreshPlayer} disabled={!stream}>Refresh</MyButton>
+            <MyButton color='blue' onClick={getVideoStreamState} disabled={!stream}>getVideoStream</MyButton>
           </ButtonGroup>
         </div>
       </Paper>
       {stream ? <>
-      <Paper elevation={1}>
-        <Typography variant="h5" gutterBottom>
-          {stream.name}
-        </Typography>
-      </Paper> 
-      <Paper elevation={1}>
-        <ReactPlayer
-          className='react-player'
-          ref={player} id="player"
-          url={stream.hls}
-          width='100%'
-          height='100%'
-        />
-      </Paper></> : null}
+        <Paper elevation={1}>
+          <Typography variant="h5" gutterBottom>
+            {stream.name}
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            {stream.source?.primary_server}/{stream.source?.stream_name}
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            {stream.hls}
+          </Typography>
+        </Paper> 
+        {stream.hls &&
+          <Paper>
+          </Paper>}
+        {stream.state === LIVE_STREAM_STATES.STARTED && stream.started?
+          <Paper elevation={1}>
+            <ReactPlayer
+              className='react-player'
+              ref={player}
+              id="player"
+              url={stream.hls}
+              width='100%'
+              height='100%'
+            />
+          </Paper>
+        : null} 
+        </> 
+      : null}
     </>
     );
 }
--- src/api.js
+++ src/api.js
@@ -5,32 +5,42 @@ const wowza_headers = {
   'wsc-api-key': wowza_api_key,
   'wsc-access-key': wowza_access_key,
 }
+const live_streams_url = `${wowza_host}/api/${wowza_version}/live_streams`;
 
 const end_points = {
   create_live_stream: {
-    url: () => `${wowza_host}/api/${wowza_version}/live_streams`,
+    url: () => `${live_streams_url}`,
     method: 'POST'
   },
   start_live_stream: {
-    url: (live_stream_id) => `${wowza_host}/api/${wowza_version}/live_streams/${live_stream_id}/start`,
+    url: (live_stream_id) => `${live_streams_url}/${live_stream_id}/start`,
     method: 'PUT'
   },
   get_live_stream: {
-    url: (live_stream_id) => `${wowza_host}/api/${wowza_version}/live_streams/${live_stream_id}`,
+    url: (live_stream_id) => `${live_streams_url}/${live_stream_id}`,
     method: 'GET'
   },
   stop_live_stream: {
-    url: (live_stream_id) => `${wowza_host}/api/${wowza_version}/live_streams/${live_stream_id}/stop`,
+    url: (live_stream_id) => `${live_streams_url}/${live_stream_id}/stop`,
     method: 'PUT'
   },
   update_live_stream: {
-    url: (live_stream_id) => `${wowza_host}/api/${wowza_version}/live_streams/${live_stream_id}`,
+    url: (live_stream_id) => `${live_streams_url}/${live_stream_id}`,
     method: 'PATCH'
   },
   get_all_streams: {
-    url: () => `${wowza_host}/api/${wowza_version}/live_streams`,
+    url: () => `${live_streams_url}`,
     method: 'GET'
+  },
+  get_live_state: {
+    url: (live_stream_id) => `${live_streams_url}/${live_stream_id}/state`,
+    method: 'GET',
+  },
+  get_video_stream: {
+    url: url => url,
+    method: 'GET',
   }
+
 }
 
 const getResponse = async (url, method, body) => {
@@ -49,16 +59,19 @@ const live_stream_body =  {
     "aspect_ratio_height": 1080,
     "aspect_ratio_width": 1920,
     "billing_mode": "pay_as_you_go",
-    "broadcast_location": "us_west_california",
+    "broadcast_location": "us_west_california", // region?
     "closed_caption_type": "none",
-    "delivery_method": "push",
+    "delivery_method": "push", // pull = source_url is required
     "encoder": "other_rtmp", // stream encoder
     "hosted_page": true,
     "hosted_page_sharing_icons": true,
     "name": "MyLiveStream", // stream name (maximum 200 characters)
     "player_responsive": true,
     "player_type": "wowza_player",
-    "transcoder_type": "transcoded"
+    "transcoder_type": "transcoded",
+    "recording": true, // false
+    "player_video_poster_image": 'https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png',
+    "disable_authentication": true,
   }
 };
 
@@ -85,4 +98,36 @@ export const stopStream = (id) => {
 export const getStream = (id) => {
   const { url, method } = end_points.get_live_stream;
   return getResponse(url(id), method);
+}
+
+export const patchStream = (id, body) => {
+  const { url, method } = end_points.update_live_stream;
+  return getResponse(url(id), method, body);
+} 
+
+export const getStreamState = (id) => {
+  const { url, method } = end_points.get_live_state;
+  return getResponse(url(id), method);
+}
+export const getVideoStream = (link) => {
+  const { url, method } = end_points.get_video_stream;
+  return getResponse(url(link), method);
+}
+
+export const checkVideo = async url => {
+  try {
+    const resp = await fetch(url, {
+      headers: {
+        //'Content-Type' : 'application/json',
+        // "Cache-Control": "no-cache",
+        //"Access-Control-Allow-Origin": "*",
+      },
+      method: 'GET'
+    });
+    console.log('check video result ', resp.ok, resp.status);
+    return resp.status === 200 || resp.ok;
+  } catch (e) {
+    console.log('check video error ', e);
+    return false;
+  }
 }
